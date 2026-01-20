5be96cfb7f35a894c7628696d9be3ba1196a5aa8
SkolaVDF <klubovnavdf@gmail.com>
2026-01-20 14:50:25 +0100

    Napsan kod pro ovladani relatka

---
+++ tests/rele.py
@@ -0,0 +1,22 @@
+# neotestovano !!!!
+
+from machine import Pin
+
+
+class Rele:
+    def __init__(self, pin):
+        """
+        Pin, kde je zapojeno nase rele. 
+        """
+        self.pin = Pin(pin, Pin.OUT)
+
+    def relay_on(self):
+        self.pin.value(1) 
+
+    def relay_off(self):
+        self.pin.value(0)  
+
+
+r = Rele(5)
+
+
