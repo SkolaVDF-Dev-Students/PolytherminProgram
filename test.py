69e072437dd1da18896879ec2e14b394e0e51b2f
SkolaVDF <klubovnavdf@gmail.com>
2026-01-26 15:38:47 +0100

    Added wanna be text

--- test.py
+++ test.py
@@ -1,17 +1,34 @@
 from st7920 import Screen
 
+# Jednoduchý 5x7 font pro písmena A-Z
+FONT = {
+    'A': [0x0E, 0x11, 0x11, 0x1F, 0x11, 0x11, 0x11],
+    'H': [0x11, 0x11, 0x11, 0x1F, 0x11, 0x11, 0x11],
+    'O': [0x0E, 0x11, 0x11, 0x11, 0x11, 0x11, 0x0E],
+    'J': [0x1F, 0x04, 0x04, 0x04, 0x04, 0x14, 0x08],
+    '!': [0x04, 0x04, 0x04, 0x04, 0x04, 0x00, 0x04],
+}
+
+def draw_char(lcd, plot, char, x, y):
+    if char not in FONT:
+        return x + 6
+    
+    for row, byte in enumerate(FONT[char]):
+        for col in range(5):
+            if byte & (1 << (4 - col)):
+                plot(x + col, y + row)
+    
+    return x + 6  # Posun na další znak
+
+def draw_text(lcd, plot, text, x, y):
+    for char in text:
+        x = draw_char(lcd, plot, char, x, y)
+
+# Test
 lcd = Screen()
 lcd.clear()
 plot = lcd.create_plotter()
 
-size = 8
-
-for row in range(0, 64 // size):
-    for col in range(0, 128 // size):
-        if (row + col) % 2 == 0:
-            for x in range(col * size, (col+1) * size):
-                for y in range(row * size, (row+1) * size):
-                    plot(x, y)
+draw_text(lcd, plot, "AHOJ!", 10, 10)
 
 lcd.redraw()
-print("Šachovnice!")
