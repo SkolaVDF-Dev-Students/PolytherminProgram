46c45d15a0412110a6a6aa3171f70682a5079563
SkolaVDF <klubovnavdf@gmail.com>
2026-02-18 18:00:34 +0100

    rewrote display render for the esp32 s3. there is none avalaible so generated ai sloppy shit that actually works :)

--- tests/lcd.py
+++ tests/lcd.py
@@ -1,6 +1,4 @@
 from tests.st7920 import Screen
-from machine import Pin
-
 
 class Lcd:
     FONT = {
@@ -49,20 +47,21 @@ class Lcd:
     }
 
     def __init__(self):
-        self.lcd = Screen(sck=Pin(12), mosi=Pin(11), miso=Pin(14), slaveSelectPin=Pin(10), resetDisplayPin=Pin(13))
+        # ESP32-S3: SCK=12, MOSI=11, CS=10, RST=13
+        self.lcd = Screen(sck=12, mosi=11, cs=10, rst=13)
         self.lcd.clear()
         self.plot = self.lcd.create_plotter()
 
     def clear(self):
-        """Clear displaye"""
+        """Vymaže framebuffer displeje"""
         self.lcd.clear()
 
     def show(self):
-        """Zobrazeni vseho co jsme setupovali"""
+        """Vykreslí obsah framebufferu na fyzický displej"""
         self.lcd.redraw()
 
     def draw_text(self, text, x, y):
-        """Vykreslení textu na souradnicich x, y"""
+        """Vykreslí text na zadaných souřadnicích"""
         for char in text.upper():
             if char in self.FONT:
                 for row, byte in enumerate(self.FONT[char]):
@@ -73,7 +72,7 @@ class Lcd:
         return x
 
     def draw_line(self, x1, y1, x2, y2):
-        """Vykresleni cary od x, y do x, y"""
+        """Algoritmus pro vykreslení čáry"""
         dx = abs(x2 - x1)
         dy = abs(y2 - y1)
         sx = 1 if x1 < x2 else -1
@@ -93,7 +92,7 @@ class Lcd:
                 y1 += sy
 
     def draw_rect(self, x1, y1, x2, y2):
-        """Vykresleni tvaru"""
+        """Vykreslení obdélníku"""
         for x in range(x1, x2 + 1):
             self.plot(x, y1)
             self.plot(x, y2)
@@ -102,45 +101,41 @@ class Lcd:
             self.plot(x2, y)
     
     def draw_heating(self):
-        """Vykresli heating menicko dole."""
+        """Vykreslí spodní lištu pro vytápění"""
         self.draw_line(0, 48, 127, 48)
         self.draw_text("HEATING", 5, 53)
         self.draw_text("%", 120, 53)
         self.draw_text("99", 107, 53)
 
-
     def draw_menu_lines(self):
-        """Vykresli cary v menu."""
+        """Vykreslí oddělovací čáry v menu"""
         self.draw_line(0, 16, 127, 16)
         self.draw_line(0, 32, 127, 32)
         self.draw_line(0, 48, 127, 48)
 
     def draw_menu(self, m1, m2, m3):
+        """Vykreslí texty tří položek menu"""
         self.draw_text(m1, 5, 5)
         self.draw_text(m2, 5, 21)
         self.draw_text(m3, 5, 37)
 
-
     def draw_menu_arrows(self, second):
-        """Vykresli menu arrows"""
+        """Vykreslí navigační šipky v menu"""
         self.draw_text("\x02", 120, 5)
         self.draw_text("\x01", 120, 21)
         if second:
             self.draw_text("\x01", 120, 37)
 
-
     def draw_heat_warning(self, x, y, t1):
-        """Vykresli heating warning podle teploty"""
+        """Vykreslí ikonu podle aktuální teploty"""
         cold_icon = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 120, 0, 8, 65, 1, 2, 84, 24, 34, 129, 2, 214, 24, 34, 129, 2, 84, 24, 33, 1, 2, 0, 24, 32, 1, 2, 0, 24, 32, 1, 2, 0, 24, 32, 1, 50, 0, 35, 16, 2, 121, 0, 79, 200, 4, 252, 128, 79, 200, 4, 252, 128, 71, 136, 2, 1, 0, 16, 32, 0, 252, 0]
         heating_icon = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 120, 56, 8, 71, 193, 2, 254, 24, 35, 129, 2, 56, 24, 35, 129, 2, 56, 24, 35, 129, 50, 56, 27, 35, 129, 50, 0, 27, 33, 193, 50, 62, 27, 35, 225, 50, 62, 35, 19, 226, 121, 62, 79, 201, 196, 252, 156, 79, 200, 4, 252, 156, 71, 137, 194, 1, 28, 16, 32, 0, 252, 0]
         hot_icon = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 120, 0, 8, 64, 1, 2, 0, 27, 32, 33, 50, 6, 27, 40, 193, 50, 216, 27, 39, 1, 50, 32, 27, 32, 1, 50, 0, 27, 33, 193, 50, 62, 27, 35, 225, 50, 62, 35, 19, 226, 121, 62, 79, 201, 196, 252, 156, 79, 200, 4, 252, 156, 71, 137, 194, 1, 28, 16, 32, 0, 252, 0]
 
         if t1 < 20:
             icon = cold_icon
-        
-        elif t1 > 20 and t1 < 300:
+        elif t1 < 300:
             icon = heating_icon
-
         else:
             icon = hot_icon
 
@@ -155,7 +150,7 @@ class Lcd:
                         self.plot(x + col, y + row)
 
     def draw_scroll(self, index):
-        """Vykresli scroll bar. Nutne zavolat v kazdem loopu. Index se vola z enkoderu."""
+        """Vykreslí rámeček (scrollbar) kolem aktivní položky menu"""
         if index < 0 or index > 3:
             return
         
@@ -177,5 +172,5 @@ class Lcd:
             self.plot(127, y)
 
     def draw_heat_settings(self, goal_temp):
-        """Vykresleni heating nastaveni"""
+        """Vykreslí nastavení cílové teploty"""
         self.draw_text(f"GOAL TEMP: {goal_temp}", 30, 25)
